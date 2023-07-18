903085a31adbcf05a7e21c3927ff52768e63493e
Victor Recio <vsrecio@macmini.local>
2023-07-18 13:56:01 -0400

    cambios en if

---
+++ control_strcuture.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+//func main()  {
+//  for i := 1; i <= 100; i ++ {
+//    if i % 3 == 0 { // Si i es un numero divisible entre 3
+//      fmt.Println(i)
+//    }
+//  }
+//
+//}
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
+}
